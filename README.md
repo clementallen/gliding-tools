2bdc9f81afd9754e6c59f757281f3c8293712e73
Clement Allen <clementallen313@gmail.com>
2015-06-10 17:06:15 +0100

    Whitespace and read update

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 This is an ever expanding collection of useful gliding tools made to practice coding.
 
-<a href="https://codeclimate.com/github/clementallen/Gliding-tools"><img src="https://codeclimate.com/github/clementallen/Gliding-tools/badges/gpa.svg" /></a>  <a href="http://issuestats.com/github/clementallen/Gliding-tools"><img src="http://issuestats.com/github/clementallen/Gliding-tools/badge/pr" /></a>  <a href="http://issuestats.com/github/clementallen/Gliding-tools"><img src="http://issuestats.com/github/clementallen/Gliding-tools/badge/issue" /></a>
-
-<h3>Glide range calculator</h3>
+### Glide range calculator
 
 This is a very simple site for calculating glide range.
 
@@ -14,10 +12,10 @@ It also calculates how many feet will be lost for every Km travelled.
 
 
 
-<h3>Silver launch height calculator</h3>
+### Silver launch height calculator
 
 Under BGA rules the launch height cannot be more than 1% of the claimed flight distance.
 
 This tool calculates how high your maximum launch height can be and also takes into account the height distance between two airfields.
 
-View a live version <a href="http://gliding.clementallen.com/" target="_blank">here</a>
+#### View a live version [here](http://gliding.clementallen.com/)
--- silver/silver.php
+++ silver/silver.php
@@ -23,136 +23,136 @@ SOFTWARE.
 <!DOCTYPE HTML>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Calculates max launch height for silver distance using airfield heights and the 1% rule">
-  <meta name="author" content="Clement Allen">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Calculates max launch height for silver distance using airfield heights and the 1% rule">
+    <meta name="author" content="Clement Allen">
 
-  <title>Silver launch height calculator</title>
+    <title>Silver launch height calculator</title>
 
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-  <link href="../assets/gliding.css" rel="stylesheet" type="text/css" />
-  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/gliding.css" rel="stylesheet" type="text/css" />
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
-<a  class="banner-hide" target="_blank" href="https://github.com/clementallen">
+<a    class="banner-hide" target="_blank" href="https://github.com/clementallen">
 <img class="github-fork-banner" style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/c6625ac1f3ee0a12250227cf83ce904423abf351/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png">
 </a>
 
 <?php
 if ($_POST['submit']) { //check if submit button as been clicked
 
-  $distance = $_POST['distance']; //gets height from the form
-  $originHeight = $_POST['originHeight']; //gets origin height from the form
-  $destinationHeight = $_POST['destinationHeight']; //gets destination height from the form
+    $distance = $_POST['distance']; //gets height from the form
+    $originHeight = $_POST['originHeight']; //gets origin height from the form
+    $destinationHeight = $_POST['destinationHeight']; //gets destination height from the form
 
-  function validate($input){ //validates inputs
-    if (!$input) { //if the input has not been entered
-      $error = 'Please enter a value';
+    function validate($input){ //validates inputs
+        if (!$input) { //if the input has not been entered
+            $error = 'Please enter a value';
+        }
+        elseif (!is_numeric($input)){ //if the input is not a number
+            $error = 'Please enter a number';
+        }
+        else {
+            $error = null; //if validation has passed don't define the variable
+        }
+        return $error;
     }
-    elseif (!is_numeric($input)){ //if the input is not a number
-      $error = 'Please enter a number';
+
+    function launchHeight($distance) { //calculates just max launch height
+        $kmToFeet = 3.281; //conversion maths from km to feet
+        $distancePercentKm = $distance / 100; //calculates 1% of the distance
+        $launchHeight = $distancePercentKm * $kmToFeet; //convert kilometres to feet
+        $launchHeightFeet = $launchHeight * 1000; //removes decimal point
+        $launchHeightFeet = round($launchHeightFeet); //rounds up/down to make a full number
+        return $launchHeightFeet;
     }
-    else {
-      $error = null; //if validation has passed don't define the variable
+    $launchHeightFeet = launchHeight($distance);
+
+    function airfieldHeight($launchHeightFeet, $originHeight, $destinationHeight){ //calculates max launch height using origin and destination altitudes
+        $airfieldHeight = $destinationHeight - $originHeight; //total difference between airfields
+        $maxLaunchHeight = $launchHeightFeet + $airfieldHeight; //calculates final launch height taking into account airfield heights
+        return $maxLaunchHeight;
     }
-    return $error;
-  }
+    $maxLaunchHeight = airfieldHeight($launchHeightFeet, $originHeight, $destinationHeight);
 
-  function launchHeight($distance) { //calculates just max launch height
-    $kmToFeet = 3.281; //conversion maths from km to feet
-    $distancePercentKm = $distance / 100; //calculates 1% of the distance
-    $launchHeight = $distancePercentKm * $kmToFeet; //convert kilometres to feet
-    $launchHeightFeet = $launchHeight * 1000; //removes decimal point
-    $launchHeightFeet = round($launchHeightFeet); //rounds up/down to make a full number
-    return $launchHeightFeet;
-  }
-  $launchHeightFeet = launchHeight($distance);
+    $errDistance = validate($distance); //validates distance
 
-  function airfieldHeight($launchHeightFeet, $originHeight, $destinationHeight){ //calculates max launch height using origin and destination altitudes
-    $airfieldHeight = $destinationHeight - $originHeight; //total difference between airfields
-    $maxLaunchHeight = $launchHeightFeet + $airfieldHeight; //calculates final launch height taking into account airfield heights
-    return $maxLaunchHeight;
-  }
-  $maxLaunchHeight = airfieldHeight($launchHeightFeet, $originHeight, $destinationHeight);
+    if($_POST['originHeight'] && $_POST['destinationHeight']){ //if airfield heights have been submitted
+        $errOriginHeight = validate($originHeight); //validates origin height
+        $errDestinationHeight = validate($destinationHeight); //validates destination height
 
-  $errDistance = validate($distance); //validates distance
-
-  if($_POST['originHeight'] && $_POST['destinationHeight']){ //if airfield heights have been submitted
-    $errOriginHeight = validate($originHeight); //validates origin height
-    $errDestinationHeight = validate($destinationHeight); //validates destination height
-
-    if(!$errDistance && !$errOriginHeight && !$errDestinationHeight){ //if validation passes print out the result
-      $result = '<div class="alert alert-success">Maximum launch height is ' . $maxLaunchHeight . 'ft.<br />And without altitude calculations it is ' . $launchHeightFeet . 'ft.</div>';
+        if(!$errDistance && !$errOriginHeight && !$errDestinationHeight){ //if validation passes print out the result
+            $result = '<div class="alert alert-success">Maximum launch height is ' . $maxLaunchHeight . 'ft.<br />And without altitude calculations it is ' . $launchHeightFeet . 'ft.</div>';
+        }
     }
-  }
 
-  elseif (!$_POST['originHeight'] && !$_POST['destinationHeight']) { // If there are no errors print out result
-    $result = '<div class="alert alert-success">Maximum launch height is ' . $maxLaunchHeight . 'ft.</div>';
-  }
+    elseif (!$_POST['originHeight'] && !$_POST['destinationHeight']) { // If there are no errors print out result
+        $result = '<div class="alert alert-success">Maximum launch height is ' . $maxLaunchHeight . 'ft.</div>';
+    }
 }
 ?>
 
-  <div class="container">
+    <div class="container">
 
-      <div class="row">
-        <div class="col-lg-8 col-lg-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
-          <div class="panel panel-default center-text">
-            <h1>Silver launch height calculator</h1>
-            <br />
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
+                    <div class="panel panel-default center-text">
+                        <h1>Silver launch height calculator</h1>
+                        <br />
 
-              <form novalidate class="form-horizontal" role="form" method="post">
+                            <form novalidate class="form-horizontal" role="form" method="post">
 
-                <div class="form-group">
-                <label for="distance" class="col-sm-3 control-label center-block">Flight distance</label>
-                <div class="col-sm-9">
-                <input type="text" class="form-control" id="distance" name="distance" placeholder="Enter flight distance in km" value="<?php if(!isset($errDistance)){echo$distance;} ?>">
-                <?php echo "<p class='text-danger'>$errDistance</p>";?>
+                                <div class="form-group">
+                                <label for="distance" class="col-sm-3 control-label center-block">Flight distance</label>
+                                <div class="col-sm-9">
+                                <input type="text" class="form-control" id="distance" name="distance" placeholder="Enter flight distance in km" value="<?php if(!isset($errDistance)){echo$distance;} ?>">
+                                <?php echo "<p class='text-danger'>$errDistance</p>";?>
+                                </div>
+                                </div>
+
+                                <div class="form-group">
+                                <label for="originHeight" class="col-sm-3 control-label">Origin altitude</label>
+                                <div class="col-sm-9">
+                                <input type="text" class="form-control" id="originHeight" name="originHeight" placeholder="Enter origin altitude in feet (optional)" value="<?php if(!isset($errOriginHeight)){echo$originHeight;} ?>">
+                                <?php echo "<p class='text-danger'>$errOriginHeight</p>";?>
+                                </div>
+                                </div>
+
+                                <div class="form-group">
+                                <label for="destinationHeight" class="col-sm-3 control-label">Destination altitude</label>
+                                <div class="col-sm-9">
+                                <input type="text" class="form-control" id="destinationHeight" name="destinationHeight" placeholder="Enter destination altitude in feet (optional)" value="<?php if(!isset($errDestinationHeight)){echo$destinationHeight;} ?>">
+                                <?php echo "<p class='text-danger'>$errDestinationHeight</p>";?>
+                                </div>
+                                </div>
+
+                                <div class="form-group">
+                                <div class="col-sm-9 col-sm-offset-3">
+                                <input id="submit" name="submit" type="submit" value="Calculate" class="btn btn-primary">
+                                </div>
+                                </div>
+
+                                <div class="form-group">
+                                <div class="col-sm-9 col-sm-offset-3">
+                                <?php echo $result; ?>
+                                </div>
+                                </div>
+
+                            </form>
+
+                    </div>
                 </div>
-                </div>
+            </div>
 
-                <div class="form-group">
-                <label for="originHeight" class="col-sm-3 control-label">Origin altitude</label>
-                <div class="col-sm-9">
-                <input type="text" class="form-control" id="originHeight" name="originHeight" placeholder="Enter origin altitude in feet (optional)" value="<?php if(!isset($errOriginHeight)){echo$originHeight;} ?>">
-                <?php echo "<p class='text-danger'>$errOriginHeight</p>";?>
-                </div>
-                </div>
+            <div class="col-lg-8 col-lg-offset-2 col-sm-10 col-sm-offset-1 col-xs-12" id="footer">
+            <p>Created by <a target="_blank" href="http://clementallen.com">Clement Allen</a> - <?php echo date('Y'); ?>.</p>
+            </div>
 
-                <div class="form-group">
-                <label for="destinationHeight" class="col-sm-3 control-label">Destination altitude</label>
-                <div class="col-sm-9">
-                <input type="text" class="form-control" id="destinationHeight" name="destinationHeight" placeholder="Enter destination altitude in feet (optional)" value="<?php if(!isset($errDestinationHeight)){echo$destinationHeight;} ?>">
-                <?php echo "<p class='text-danger'>$errDestinationHeight</p>";?>
-                </div>
-                </div>
-
-                <div class="form-group">
-                <div class="col-sm-9 col-sm-offset-3">
-                <input id="submit" name="submit" type="submit" value="Calculate" class="btn btn-primary">
-                </div>
-                </div>
-
-                <div class="form-group">
-                <div class="col-sm-9 col-sm-offset-3">
-                <?php echo $result; ?>
-                </div>
-                </div>
-
-              </form>
-
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-8 col-lg-offset-2 col-sm-10 col-sm-offset-1 col-xs-12" id="footer">
-      <p>Created by <a target="_blank" href="http://clementallen.com">Clement Allen</a> - <?php echo date('Y')?>.</p>
-      </div>
-
-  </div><!-- /container -->
+    </div><!-- /container -->
 
 </body>
 
