c62b47112b26a3f0b9fdff94ee045b9ff912516d
Clement Allen <clementallen313@gmail.com>
2015-06-23 17:08:16 +0100

    Added number of gliders live update

--- assets/js/bgahandicaps.js
+++ assets/js/bgahandicaps.js
@@ -2,7 +2,11 @@ var options = {
     valueNames: [ 'glider', 'handicap' ],
     page: [306]
 };
-
 var gliderList = new List('handicap-search', options);
 
-$("p.total span").append($("tbody.list tr").length);
+
+$("p.total span").text($('tbody.list tr').length);
+
+document.getElementById('glider-search').onkeyup = function() {
+    $('p.total span').text($('tbody.list tr').length);
+}
