8ab0824558b0adfdd29f9ee2a1dd78d751bbb79e
Clement Allen <clementallen313@gmail.com>
2015-06-23 16:28:41 +0100

    Assets folder sorting

--- tests/silverTest.php
+++ tests/silverTest.php
@@ -41,18 +41,18 @@ class SilverTest extends PHPUnit_Framework_TestCase {
 
     public function testLaunchHeightPasses() {
 
-        $this->assertEquals(3281, $this->Silver->launchHeight(100) );
-        $this->assertEquals(6562, $this->Silver->launchHeight(200) );
-        $this->assertEquals(9843, $this->Silver->launchHeight(300) );
-        $this->assertEquals(13124, $this->Silver->launchHeight(400) );
+        $this->assertEquals( 3281, $this->Silver->launchHeight(100) );
+        $this->assertEquals( 6562, $this->Silver->launchHeight(200) );
+        $this->assertEquals( 9843, $this->Silver->launchHeight(300) );
+        $this->assertEquals( 13124, $this->Silver->launchHeight(400) );
     }
 
     public function testAirfieldHeightPasses() {
 
-        $this->assertEquals(2881, $this->Silver->airfieldHeight(3281, 500, 100) );
-        $this->assertEquals(6962, $this->Silver->airfieldHeight(6562, 100, 500) );
-        $this->assertEquals(9643, $this->Silver->airfieldHeight(9843, 400, 200) );
-        $this->assertEquals(13324, $this->Silver->airfieldHeight(13124, 200, 400) );
+        $this->assertEquals( 2881, $this->Silver->airfieldHeight(3281, 500, 100) );
+        $this->assertEquals( 6962, $this->Silver->airfieldHeight(6562, 100, 500) );
+        $this->assertEquals( 9643, $this->Silver->airfieldHeight(9843, 400, 200) );
+        $this->assertEquals( 13324, $this->Silver->airfieldHeight(13124, 200, 400) );
     }
 
 
