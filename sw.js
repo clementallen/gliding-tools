1e62ad22909558ec2ef1a565178a4e0d8ec13c6a
Clement Allen <clementallen313@gmail.com>
2016-10-02 14:58:39 +0100

    Added Service Worker (#22)
    * Added caching Service Worker
    
    * Added manifest

---
+++ sw.js
@@ -0,0 +1,28 @@
+importScripts('node_modules/sw-toolbox/sw-toolbox.js');
+
+var precacheList = [
+    './',
+    './glide',
+    './bgahandicaps',
+    './handicap',
+    './silver',
+    './weight',
+    './assets/js/config.js',
+    './assets/js/main.js',
+    './assets/js/validators.js',
+    './assets/js/glide.js',
+    './assets/js/bgaHandicaps.js',
+    './assets/js/handicap.js',
+    './assets/js/silver.js',
+    './assets/js/weight.js',
+    './assets/js/vendor/jquery.js',
+    './assets/js/vendor/list.js',
+    './assets/js/vendor/require.js',
+    './assets/bgahandicaps.json',
+    './assets/css/bootstrap.min.css',
+    './assets/css/gliding.css'
+];
+
+toolbox.precache(precacheList);
+
+toolbox.router.get('/(.*)', toolbox.fastest);
